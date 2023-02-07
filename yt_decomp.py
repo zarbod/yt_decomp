82470e650b0accbb30380befbada65624864658f
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2023-02-07 01:32:48 -0600

    Created chapter names list

--- yt_decomp.py
+++ yt_decomp.py
@@ -10,18 +10,36 @@ def stamp_to_sec(stamp):
     units.reverse()
     return sum(list(map(operator.mul, units, [1, 60, 3600])))
 
-def process_times(vid):
+def process_chapters(vid):
     desc = YouTube(vid).description
-    timestamp_regex = re.compile(r'[0-9]?[0-9]?:?[0-9]?[0-9]:[0-9][0-9]')
+    timestamp_regex = re.compile(r'[0-9]?[0-9]?:?[0-9]?[0-9]:[0-9][0-9]\s.*\n')
     stmp_list = timestamp_regex.findall(desc)
-    fin_list = []
-    for x in stmp_list:
-        if not (x in fin_list):
-            fin_list.append(x)
-    
-    return list(map(stamp_to_sec, fin_list))
-
+    stmp_list = map(lambda x : x.split(' ', 1), stmp_list)
+    stmp_list = map(lambda x : [x[0], x[1][:-1]], stmp_list)
+    return list(stmp_list)
+    # fin_list = []
+    # for x in stmp_list:
+    #     if not (x in fin_list):
+    #         fin_list.append(x)
+    # 
+    # return list(map(stamp_to_sec, fin_list))
 
 vid = input("Enter a YouTube Video URL: ")
 
-times_list = process_times(vid)
+chapters = process_chapters(vid)
+
+times = list(map(stamp_to_sec, list(map(lambda x : x[0], chapters))))
+names = list(map(lambda x : x[1], chapters))
+
+print(times)
+print(names)
+
+ydl_opts = {
+        'format' : 'bestaudio/best',
+        'postprocessors' : [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+}
+
